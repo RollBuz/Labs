521bbe9055190c51d987c2e56ae97febb442742c
RollBuz <r_buzhigitov@kbtu.kz>
2025-01-25 09:55:08 +0500

    Файл добавлен

---
+++ PythonVariables.py
@@ -0,0 +1,14 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+z = 17        # x is of type int
+z = "Rollan"  # x is now of type str
+print(z)
+
+a = 4
+A = "RolBuz"
+#A will not overwrite a
+print(a)
+print(A)
