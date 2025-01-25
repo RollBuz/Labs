a3f2312bb67b33130e4d4102d2e4b927d2980388
RollBuz <r_buzhigitov@kbtu.kz>
2025-01-25 09:55:08 +0500

    Файл добавлен

---
+++ PythonDataTypes.py
@@ -0,0 +1,74 @@
+"""Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""
+x = "Hello World"
+
+print(x)
+
+print(type(x))
+
+x = 20
+
+print(x)
+
+print(type(x))
+
+x = 20.5
+
+print(x)
+
+print(type(x))
+
+x = 1j
+
+print(x)
+
+print(type(x)) 
+
+x = ["apple", "banana", "cherry"]
+
+print(x)
+
+print(type(x)) 
+
+x = ("apple", "banana", "cherry")
+
+print(x)
+
+print(type(x))
+
+x = range(6)
+
+print(x)
+
+print(type(x))
+
+x = {"name" : "John", "age" : 36}
+
+print(x)
+
+print(type(x))
+
+x = {3, 3, 4, 5}
+
+print(x)
+
+print(type(x))
+
+x = True
+
+print(x)
+
+print(type(x))
+
+x = None
+
+print(x)
+
+print(type(x))
