433bd287f3162f7fae22b8f74b409c018eba4ed7
RollBuz <r_buzhigitov@kbtu.kz>
2025-01-25 12:53:54 +0500

    Добавлены файлы после восстановления

---
+++ Lab2/BooleanValues.py
@@ -0,0 +1,25 @@
+#T | F
+print(10 > 9) #True
+print(10 == 9) #False
+print(10 < 9) #False
+
+#If
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+#Bool on values
+print(bool("Hello")) #True
+print(bool()) #False because there is no information
+
+"""
+Any string is True, except empty strings.
+
+Any number is True, except 0.
+
+Any list, tuple, set, and dictionary are True, except empty ones.
+"""
